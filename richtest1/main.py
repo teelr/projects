e92a4bf48048688d5975d0867cc5898c40347a3a
teelr <richteel@hotmail.com>
2025-05-06 06:13:04 -0500

    init commit

---
+++ richtest1/main.py
@@ -0,0 +1,22 @@
+def add(a, b):
+    return a + b
+
+
+def multiply(x, y):
+    result = x * y
+    return result
+
+
+def subtract(x, y):
+    result = x - y
+    return result
+
+
+def main():
+    print("This Rich Teel calling...")
+    print(add(1, 2))
+    print(multiply(3, 4))
+    print(subtract(5, 2))
+
+
+main()
